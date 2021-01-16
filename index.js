56226fd437116ebd3e91a8987452b4b92fd85e16
EiskalterFreund <72300200+EiskalterFreund@users.noreply.github.com>
2021-01-16 09:50:30 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+const nodeactyl_v1 = require('nodeactyl-v1-support')
+const node = nodeactyl_v1.Client
+
+node.login('https://YOURHOST.example', 'YOUR API CREDENTIALS', (logged_in, msg) => {
+    console.log('Log in status:', logged_in)
+    loadServer()
+})
+
+var servers = 
+[
+    { name: "CHWEPPEN - Webserver (2)", id: "6107030d" },
+    { name: "CHWEPPEN - Webserver (1)", id: "734744bb" },
+    { name: "McProject", id: "5a13c1b9" },
+    { name: "Minecraft OneBlock", id: "88756bf5" }
+]
+
+function loadServer() {
+    console.log("------------")
+	
+    for (var i = 0; i < servers.length; i++) {
+        checkServer(servers[i])
+    }
+    setTimeout(loadServer, 5000)
+}
+
+function checkServer(server) {
+    node.getServerStatus(server.id).then((response) => {
+        if (response != null) {
+            if (response == "running") {
+                console.log("Server " + server.name + " is running")
+            } else if (response == "starting") {
+                console.log("Server " + server.name + " is starting")
+            } else if (response == "offline") {
+                console.log("Server " + server.name + " is offline, rebooting")
+                rebootServer(server)
+            } else if (response == "stopping") {
+                console.log("Server " + server.name + " is stopping")
+            }
+        } else {
+            console.log("Some error occurred.")
+        }
+    }).catch((error) => {
+        console.log("Server " + server.name + " isn't loading (node down?)")
+    })
+}
+
+function rebootServer(server) {
+    node.startServer(server.id).then((response) =>  {
+        console.log(response)
+    }).catch((error) => {
+        console.log("Server " + server.name + " isn't loading (node down?)")
+    })
+}
